560e88558f987093caf945a4042c216bf662beef
Elizabetttz <karenelizabet2828@gmail.com>
2025-10-02 18:24:30 -0500

    Implementaci�n de tema oscuro y claro- fixx

---
+++ src/app/components/home/facturacion/home-factur.component/home_factur.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-home-factur',
+    templateUrl: './home_factur.component.html',
+    styleUrls: ['./home_factur.component.scss']
+})
+export class HomeFacturComponent {
+    // Component logic goes here
+}
